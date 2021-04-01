518a8e9e2791ff92c89af30e655a24731ca0fae2
heyi <249462645@qq.com>
2021-04-01 10:32:34 +0800

    test

---
+++ src/test/java/com/xiaohe/mamono/MamonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohe.mamono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
